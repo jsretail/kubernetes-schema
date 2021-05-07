32ce37c7d337f41b28e6b7e0acf726eb15b46dfd
Thomas Hunter <thunter1000@gmail.com>
2021-05-07 20:50:44 +0100

    Add script to generate schema for multiple versions

---
+++ generate.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+set -e
+
+
+# AKS supported versions 2021-05-07
+# 1.19.9, 1,19.7, 1.18.17, 1.18.14
+
+declare -a all=(
+  v1.19.9
+  v1.19.7
+  v1.18.17
+  v1.18.14
+)
+
+for version in "${all[@]}"
+do
+  schema="https://raw.githubusercontent.com/kubernetes/kubernetes/${version}/api/openapi-spec/swagger.json"
+  prefix="https://kubernetesjsonschema.dev/${version}/_definitions.json"
+
+  openapi2jsonschema -o "${version}-standalone-strict" --expanded --kubernetes --stand-alone --strict "${schema}"
+  openapi2jsonschema -o "${version}-standalone" --expanded --kubernetes --stand-alone "${schema}"
+  openapi2jsonschema -o "${version}-local" --expanded --kubernetes "${schema}"
+  openapi2jsonschema -o "${version}" --expanded --kubernetes --prefix="${prefix}" "${schema}"
+  openapi2jsonschema -o "${version}-standalone-strict" --kubernetes --stand-alone --strict "${schema}"
+  openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone "${schema}"
+  openapi2jsonschema -o "${version}-local" --kubernetes "${schema}"
+  openapi2jsonschema -o "${version}" --kubernetes --prefix="${prefix}" "${schema}"
+done
