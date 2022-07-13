7f2eaf4d331d60917ed08da9ac5e089c7f9ae57d
Thomas Hunter <thunter1000@gmail.com>
2022-07-13 16:20:42 +0100

    chore(generate): Correct prefix url to github user content address

--- generate.sh
+++ generate.sh
@@ -21,15 +21,17 @@ declare -a all=(
 for version in "${all[@]}"
 do
   schema="https://raw.githubusercontent.com/kubernetes/kubernetes/${version}/api/openapi-spec/swagger.json"
-  prefix="https://kubernetesjsonschema.dev/${version}/_definitions.json"
+  prefix="https://raw.githubusercontent.com/jsretail/kubernetes-schema/main/${version}/_definitions.json"
 
-  openapi2jsonschema -o "${version}-standalone-strict" --expanded --kubernetes --stand-alone --strict "${schema}" &
-  openapi2jsonschema -o "${version}-standalone" --expanded --kubernetes --stand-alone "${schema}" &
-  openapi2jsonschema -o "${version}-local" --expanded --kubernetes "${schema}" &
-  openapi2jsonschema -o "${version}" --expanded --kubernetes --prefix="${prefix}" "${schema}" &
-  openapi2jsonschema -o "${version}-standalone-strict" --kubernetes --stand-alone --strict "${schema}" &
-  openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone "${schema}" &
-  openapi2jsonschema -o "${version}-local" --kubernetes "${schema}" &
-  openapi2jsonschema -o "${version}" --kubernetes --prefix="${prefix}" "${schema}" &
-  wait
+  (
+    openapi2jsonschema -o "${version}-standalone-strict" --expanded --kubernetes --stand-alone --strict "${schema}" &
+    openapi2jsonschema -o "${version}-standalone" --expanded --kubernetes --stand-alone "${schema}" &
+    openapi2jsonschema -o "${version}-local" --expanded --kubernetes "${schema}" &
+    openapi2jsonschema -o "${version}" --expanded --kubernetes --prefix="${prefix}" "${schema}" &
+    openapi2jsonschema -o "${version}-standalone-strict" --kubernetes --stand-alone --strict "${schema}" &
+    openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone "${schema}" &
+    openapi2jsonschema -o "${version}-local" --kubernetes "${schema}" &
+    openapi2jsonschema -o "${version}" --kubernetes --prefix="${prefix}" "${schema}" &
+    wait
+  )
 done
